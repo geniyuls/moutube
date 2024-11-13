b0cabafe84d8a92dad2412fb0d91aaf6f015ff00
geniyuls <yulisis123@naver.com>
2024-11-13 15:05:33 +0900

    first

---
+++ src/test/java/com/jykim/moutube/MoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jykim.moutube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoutubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
